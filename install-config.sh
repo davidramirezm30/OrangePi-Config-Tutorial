a067487b4381cf5a08a0e793475762b6e5732cc8
davidramirezm30 <davidramirez.drm@gmail.com>
2018-10-28 00:26:29 +0200

    Update install-config.sh

--- install-config.sh
+++ install-config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 apt-get update
 apt-get upgrade -y
-apt autoremove
+apt autoremove -y
 apt-get install python3
 apt-get install python-setuptools
 apt-get install python-dev python3-dev
