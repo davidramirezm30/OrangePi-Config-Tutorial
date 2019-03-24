bf5025414f22c073a07628da0b1dbef4ca4f52c4
davidramirezm30 <davidramirez.drm@gmail.com>
2019-03-24 13:46:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt autoremove -y
 sudo apt-get install mercurial -y
-sudo apt-get install build-essential python3 python-dev python3-dev python-distlib python-setuptools python-pip python-smbus python3-pip python-wheel python-smbus libgdal-dev -y
+sudo apt-get install build-essential python3 python-dev python3-dev python-distlib python-setuptools python-pip python-smbus python3-pip python-wheel python-smbus python-numpy python-opencv libgdal-dev -y
 sudo apt-get install i2c-tools
 sudo apt-get install libfreetype6-dev
 sudo apt-get install sysinfo
-pip uninstall pillow
-pip install --no-cache-dir pillow
-sudo pip install psutil
+sudo pip uninstall pillow
+sudo pip install --no-cache-dir pillow
+sudo pip install psutil pigpio paho-mqtt python-uinput bluedot kinematic 
 sudo adduser pi i2c
 echo i2c-dev >> /etc/modules
 echo i2c-algo-bit >> /etc/modules-load.d/i2c.conf
